6c68fecc711339b28e7ce176283e73750218452a
mohitkumar7925 <33751404+mohitkumar7925@users.noreply.github.com>
2022-06-04 11:24:23 +0530

    Add files via upload

---
+++ precache-manifest.43adbfb7f475cd5070d6faeb8bd05cb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e98fb852862b40f2bb30",
+    "url": "/static/css/main.58c8d259.chunk.css"
+  },
+  {
+    "revision": "e98fb852862b40f2bb30",
+    "url": "/static/js/main.8a00fdba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "9426d59aa7ab270cecc19f17db99984c",
+    "url": "/static/media/newLogo.9426d59a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "a3c2d593223fabcecfcc0d6f57eb0a3e",
+    "url": "/index.html"
+  }
+];
